7796f4e9e560f094a62eef2237027d5e6b45f73b
Fatih Tüfekçi <fatihtufekci1997@gmail.com>
2022-04-25 03:16:57 +0300

    only list method allowed

--- vehicle_navigation/api/urls.py
+++ vehicle_navigation/api/urls.py
@@ -1,10 +1,7 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from vehicle_navigation.api import views as api_views
 
-router = DefaultRouter()
-router.register('navigation-record', api_views.NavigationRecordViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('navigation-record/', api_views.NavigationRecordViewSet.as_view({'get': 'list'})),
+
 ]
